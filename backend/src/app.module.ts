e389bc15b0944f304b07dc622145f62fea6915b2
farrellh1 <farrell.hauzan@gmail.com>
2024-05-14 23:43:41 +0700

    feat: add versioning

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,33 +1,18 @@
 import { Module } from '@nestjs/common';
-import { AuthModule } from './auth/auth.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { TypeOrmConfigService } from './database/typeorm-config.service';
-import { UsersModule } from './users/users.module';
-import { JwtModule } from '@nestjs/jwt';
-import { jwtConstants } from './constants';
-import { APP_GUARD } from '@nestjs/core';
-import { AuthGuard } from './auth/auth.guard';
 import { ConfigModule } from '@nestjs/config';
+import { WalletsModule } from './wallets/wallets.module';
+import { AddressesModule } from './addresses/addresses.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot(),
     TypeOrmModule.forRootAsync({ useClass: TypeOrmConfigService }),
-    JwtModule.register({
-      global: true,
-      secret: process.env.SECRET_TOKEN,
-      signOptions: { expiresIn: jwtConstants.expiresIn },
-    }),
-    AuthModule,
-    UsersModule,
+    WalletsModule,
+    AddressesModule,
   ],
   controllers: [],
-  providers: [
-    {
-      provide: APP_GUARD,
-      useClass: AuthGuard,
-    },
-  ],
 })
 
 export class AppModule {}
--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,10 +1,21 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { ValidationPipe } from '@nestjs/common';
+import { ValidationPipe, VersioningType } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // set global validation
   app.useGlobalPipes(new ValidationPipe());
+
+  // set global prefix
+  app.setGlobalPrefix('api');
+
+  // enable versioning
+  app.enableVersioning({
+    type: VersioningType.URI,
+    prefix: 'v',
+  });
   await app.listen(3000);
 }
 bootstrap();
